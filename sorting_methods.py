7dba0bcc9511f4b95ce9ab370a08b8ba8660cb5b
Sm00k3y - dr4g0n <kubasmolka1999@gmail.com>
2020-10-09 13:47:44 +0200

    Adding implementation of Quicksort

--- sorting_methods.py
+++ sorting_methods.py
@@ -1,4 +1,5 @@
 import tree
+import random
 
 
 def tree_sort(array):
@@ -19,9 +20,34 @@ def tree_sort(array):
     return sorted_array
 
 
+def quicksort(array):
+    """
+    Quicksort recursively sorts splitted arrays, one array with numbers lower 
+    and one with numbers highier than the pivot
+    """
+    if len(array) < 2:
+        return array
+
+    lower, equal, highier = [], [], []
+    pivot = array[random.randint(0, len(array)-1)]
+
+    for number in array:
+        if number < pivot:
+            lower.append(number)
+        elif number > pivot:
+            highier.append(number)
+        else:
+            equal.append(number)
+
+    return quicksort(lower) + equal + quicksort(highier)
+
+
 if __name__ == "__main__":
     unsorted_numbers = [-7.23, 19, 3.256, 9909, 0.000002, -45.23, -0.0002, 19, 0.0]
     print("Array to sort:\t", unsorted_numbers)
 
     tree_sorted_array = tree_sort(unsorted_numbers)
     print("Tree sort:\t", tree_sorted_array)
+
+    quicksorted_array = quicksort(unsorted_numbers)
+    print("Quicksort:\t", quicksorted_array)
